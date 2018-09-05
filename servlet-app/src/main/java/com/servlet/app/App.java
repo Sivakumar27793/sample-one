42c75675363adbb2eea393de6ce6e1770bf03f7e
Sivakumar27793 <sivakumar93277@gmail.com>
2018-09-05 08:46:54 +0000

    commit

---
+++ servlet-app/src/main/java/com/servlet/app/App.java
@@ -0,0 +1,13 @@
+package com.servlet.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
